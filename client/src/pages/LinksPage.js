81a813e94928d339adb7b5412a341969789b539e
kkfullsd <kir2915@gmail.com>
2020-07-17 00:44:53 +0300

    Страница LinksPage, компонент LinksList

--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useContext, useCallback, useEffect } from 'react'
+import {useHttp} from '../hooks/http.hook'
+import {AuthContext} from '../context/AuthContext'
+import {Loader} from '../components/Loader'
+import {LinksList} from '../components/LinksList'
 
 export const LinksPage = () => {
+    const [links, setLinks] = useState([])
+    const {request, loading} = useHttp()
+    const {token} = useContext(AuthContext)
+
+
+    const fetchLinks = useCallback( async ()=>{
+        try {
+            const fetched = await request('/api/link', 'GET', null, {
+                Authorization: `Bearer ${token}`
+            })
+            setLinks(fetched)
+            console.log(fetched)
+        } catch (e) {
+            console.log(e, '20')
+        }
+    }, [token, request])
+
+    useEffect(()=>{
+        fetchLinks()
+    }, [fetchLinks])
+
+
+    if (loading) {
+        return <Loader />
+    }
+
     return (
         <div>
-            <h1>LinksPage</h1>
+            {!loading && <LinksList links={links} />}
         </div>
     )
 }
